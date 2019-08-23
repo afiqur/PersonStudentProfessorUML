938bf71031da994020ad28b5cb8333509ecfe2bb
Afiqur Rahman Piash <afiqur_rahman@yahoo.com>
2019-08-23 15:06:56 +0600

    Second Commit...

--- src/ARP/PersonMain.java
+++ src/ARP/PersonMain.java
@@ -1,17 +1,30 @@
 package ARP;
 
 public class PersonMain {
-	public static void main(String[] args) {
-		Professor p = new Professor(50, "Assistant Professor");
-		System.out.println("Professor Age: " + p.get_Age());
-		p.display();
-		p.teaching();
-		p.testing();
+	Professor professor;
+	Student student;
 
-		Student s = new Student(20, "182-15-182");
-		System.out.println("Student Age: " + s.get_Age());
-		s.display();
-		s.reading();
-		s.testing();
+	public PersonMain(Professor professor, Student student) {
+		this.professor = professor;
+		this.student = student;
+	}
+
+	public static void main(String[] args) {
+
+		Professor professor = new Professor(50, "Assistant Professor");
+
+		Student student = new Student(20, "182-15-182");
+
+		PersonMain personMain = new PersonMain(professor, student);
+
+		System.out.println("Professor Age: " + personMain.professor.get_Age());
+		personMain.professor.display();
+		personMain.professor.teaching();
+		personMain.professor.testing();
+
+		System.out.println("Student Age: " + personMain.student.get_Age());
+		personMain.student.display();
+		personMain.student.reading();
+		personMain.student.testing();
 	}
 }
